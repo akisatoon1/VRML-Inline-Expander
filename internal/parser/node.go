4ce055a79a237190117a06ba90b9b1f9293f13cd
akisatoon <akisatoon@gmail.com>
2026-02-03 21:43:39 +0900

    Delete unused

--- internal/parser/node.go
+++
@@ -1,9 +0,0 @@
-package parser
-
-// InlineNode represents an Inline node found in a VRML file
-type InlineNode struct {
-	DefName   string // DEF name (optional, empty string if not defined)
-	UrlPath   string // URL path to the referenced file
-	StartLine int    // Starting line number (0-indexed)
-	EndLine   int    // Ending line number (0-indexed, inclusive)
-}
